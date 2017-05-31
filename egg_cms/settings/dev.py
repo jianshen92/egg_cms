4ed33fe070e5916368274b32e19f3f3788cab36e
Jian Shen Yap <jianshen@dangodigital.com>
2017-05-31 18:05:30 +0800

    Initial Commit

---
+++ egg_cms/settings/dev.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'h+r3+ok+olvh8l)8ha$=+#zg!j*icmbr4##x9kj(zarz@+*l5p'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
