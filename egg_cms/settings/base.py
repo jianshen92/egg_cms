51a7a0784ff01c3d060b4972f985e39181208987
Jian Shen Yap <jianshen@dangodigital.com>
2017-06-01 17:52:12 +0800

    Change db to mysql.

--- egg_cms/settings/base.py
+++ egg_cms/settings/base.py
@@ -102,10 +102,21 @@ WSGI_APPLICATION = 'egg_cms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'egg_cms',
+        'USER': 'root',
+        'PASSWORD': 'DD1010sph',
+        'HOST': '',
+        'PORT': '3306',
     }
 }
 
